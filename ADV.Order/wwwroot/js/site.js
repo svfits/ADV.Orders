89c2e884640a56d671147484260140e82057f28e
Дмитрий Афанасьев <a1d1@inbox.ru>
2020-11-09 08:39:15 +0800

    Получение и запись ответа на клиенте

--- ADV.Order/wwwroot/js/site.js
+++ ADV.Order/wwwroot/js/site.js
@@ -1,39 +1,13 @@
-﻿function viewModel() {
+﻿
+function viewModel() {
+    
+    var self = this;
+    
+    var datailOrder = $("#datailOrder").html();
+    self.datailOrder = ko.observable(datailOrder);
 
-    var idOrder = 0;
-    //var previousCount = self.datailOrder;
-
-    $.ajax({
-        url: "/Home/DatailOrder",
-        data: {
-            id: idOrder
-        },
-        type: "GET",
-        dataType: "html",
-        async: false,
-        success: function (data) {
-            self.datailOrder = ko.observable(data);
-        },
-        error: function (data) {
-            console.error("Что то произошло не так при получении datailOrder " + data);
-        }
-    });
-
-    $.ajax({
-        url: "/Home/Products",
-        data: {
-            id: idOrder
-        },
-        type: "GET",
-        dataType: "html",
-        async: false,
-        success: function (data) {
-            self.products = ko.observable(data);
-        },
-        error: function (data) {
-            console.error("Что то произошло не так при получении продуктов " + data);
-        }
-    });
+    var products = $("#products").html();
+    self.products = ko.observable(products);
 
     this.order = function () {
 
